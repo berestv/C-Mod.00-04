5e2f44b61ee8be39b5f9f64d02458e03c7367b63
berestv <bbento-e@student.42porto.com>
2024-07-02 17:13:51 +0100

    Finished megaphone.cpp

--- Module 00/ex00/megaphone.cpp
+++ Module 00/ex00/megaphone.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <cctype>
+#include <cstring>
 
-int main()
+int main(int argc, char *argv[])
 {
-	std::cout << "Hello world" << std::endl;
-	//std::cin.get();
-
+	if (argc >= 2)
+	{
+		for (int j = 1; j < argc; ++j)
+			for (size_t i = 0; i <= strlen(argv[j]); ++i)
+				std::cout << (char)toupper(argv[j][i]);
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
 }
