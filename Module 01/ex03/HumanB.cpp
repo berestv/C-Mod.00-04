1300fe5eaacd00f3d5ce63c506e075a508d4b1bd
Bernardo <bbento-e@student.42porto.com>
2024-09-09 18:42:16 +0100

    completing ex03

---
+++ Module 01/ex03/HumanB.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kali on 9/9/24.
+//
+
+#include "HumanB.hpp"
---
+++ Module 01/ex03/Weapon.cpp
@@ -0,0 +1,11 @@
+#include "Weapon.hpp"
+
+Weapon::Weapon(){ };
+
+std::string Weapon::getType (){
+	return type;
+}
+
+void Weapon::setType ( std::string tp ){
+
+}
