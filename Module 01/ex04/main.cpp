bb019199cbe059fd7f93e407153e93a8af0636a6
Bernardo <bbento-e@student.42porto.com>
2024-09-11 11:51:27 +0100

    completed ex04. Makefile deletes *.replace

--- Module 01/ex04/main.cpp
+++ Module 01/ex04/main.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
 #include <fstream>
 
-std::string scan_line (std::string line, char *s1, char *s2)
+std::string scan_line (const std::string& line, const std::string& s1, const std::string& s2)
 {
 	std::string newline;
-	int find = ((line.find(s1, s2)) != std::string::npos);
-	while (std::string)
-	{
+	size_t i = 0;
+	size_t pos;
 
+	while ((pos = line.find(s1, i)) != std::string::npos)
+	{
+		newline.append(line, i, pos - i);
+		newline.append(s2);
+		i += pos + s1.length();
 	}
-	return line;
+	newline.append(line, i, line.length() - i);
+	return newline;
 }
 
 int main (int argc, char **argv) {
 	if (argc == 4)
 	{
-		std::string str;
 		std::ifstream doc(argv[1]);
 
 		if (doc.is_open())
 		{
-			str = (std::string)argv[1] + (".replace");
-			std::ofstream out(str);
+			std::ofstream out;
 			std::string line;
+			std::string str = (std::string)argv[1] + (".replace");
 
-			out.open(str);
+			out.open(str.c_str());
 			while (std::getline(doc, line))
 				out << scan_line(line, argv[2], argv[3]) << std::endl;
 			doc.close();
