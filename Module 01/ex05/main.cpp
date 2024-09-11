b5e699ed3ff4bb91de09ba9b15baf6e6b4cfddfc
Bernardo <bbento-e@student.42porto.com>
2024-09-11 20:48:23 +0100

    I made Harl

---
+++ Module 01/ex05/main.cpp
@@ -0,0 +1,32 @@
+#include "Harl.hpp"
+
+int main (int argc, char *argv[]){
+	Harl harley;
+
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; ++i) {
+			harley.complain(argv[i]);
+		}
+		std::cout << std::endl;
+		std::cout << "Separate tests:" << std::endl;
+		std::cout << std::endl;
+	}
+	// Testing valid complaints
+	harley.complain("DEBUG");
+	std::cout << std::endl;
+	harley.complain("INFO");
+	std::cout << std::endl;
+	harley.complain("WARNING");
+	std::cout << std::endl;
+	harley.complain("ERROR");
+	std::cout << std::endl;
+
+	std::cout << std::endl;
+
+	// Testing invalid complaints
+	harley.complain("INVALID");
+	harley.complain("MORE BACON!");
+	harley.complain("123");
+	harley.complain("1");
+}
