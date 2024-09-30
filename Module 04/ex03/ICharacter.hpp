4a7c19efeb44d088a06988a3cba2a7714972ce51
Bernardo <bbento-e@student.42porto.com>
2024-09-30 16:33:04 +0100

    created all files

---
+++ Module 04/ex03/ICharacter.hpp
@@ -0,0 +1,10 @@
+#ifndef ICHARACTER_HPP
+#define ICHARACTER_HPP
+
+#include "AMateria.hpp"
+
+class ICHaracter {
+
+};
+
+#endif ICHARACTER_HPP
---
+++ Module 04/ex03/Ice.hpp
@@ -0,0 +1,10 @@
+#ifndef ICE_HPP
+#define ICE_HPP
+
+#include "AMateria.hpp"
+
+class Ice : AMateria {
+
+};
+
+#endif //ICE_HPP
