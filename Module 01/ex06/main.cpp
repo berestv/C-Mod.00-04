b0f130314df566438f2440213b6d14d5bbdf7b47
berestv <bbento-e@student.42porto.com>
2024-10-14 17:25:33 +0100

    bug fixes

--- Module 01/ex06/main.cpp
+++ Module 01/ex06/main.cpp
@@ -3,29 +3,8 @@
 int main (int argc, char *argv[]){
 	Harl harley;
 
-	if (argc > 1)
-	{
-		for (int i = 1; i < argc; ++i)
-			harley.complain(argv[i]);
-/*		std::cout << std::endl;
-		std::cout << "Separate tests:" << std::endl;
-		std::cout << std::endl;*/
-	}
-/*	// Testing valid complaints
-	harley.complain("DEBUG");
-	std::cout << std::endl;
-	harley.complain("INFO");
-	std::cout << std::endl;
-	harley.complain("WARNING");
-	std::cout << std::endl;
-	harley.complain("ERROR");
-	std::cout << std::endl;
-
-	std::cout << std::endl;
-
-	// Testing invalid complaints
-	harley.complain("INVALID");
-	harley.complain("MORE BACON!");
-	harley.complain("123");
-	harley.complain("1");*/
+	if (argc == 2)
+			harley.complain(argv[1]);
+	else
+		std::cout << "Wrong number of arguments!" << std::endl;
 }
